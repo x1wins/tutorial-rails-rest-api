ef97f63e682e9b9e13ae929bf93546e3b9eb4219
Chang-Woo Rhee <x1wins@changwoo.net>
2020-01-02 10:12:59 -0800

    Changed is_owner_object to is_owner

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   before_action :authorize_request, except: :create
   before_action :find_user, except: %i[create index]
   before_action only: [:show, :update, :destroy] do
-    is_owner_object @user ##your object
+    is_owner @user.id ##your object
   end
 
   # GET /users
@@ -20,7 +20,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      render json: @user, status: :created
+      render json: @user, status: :created, location: user_url(@user.username)
     else
       render json: { errors: @user.errors.full_messages },
              status: :unprocessable_entity
