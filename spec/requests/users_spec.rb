67c0c470affffe14b5db18cca6c3eb207fa9528f
Chang-Woo Rhee <x1wins@changwoo.net>
2020-01-03 13:41:07 -0800

    Fix user rspec error

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,36 +1,38 @@
 require 'swagger_helper'
 
 RSpec.describe 'Users API' do
-
   path '/users' do
 
-    # get('list users') do
-    #   response(200, 'successful') do
-    #
-    #     after do |example|
-    #       example.metadata[:response][:examples] = { 'application/json' => JSON.parse(response.body, symbolize_names: true) }
-    #     end
-    #     run_test!
-    #   end
-    # end
+    get('list users') do
+      tags 'User'
+      consumes 'application/json'
+      response(200, 'successful') do
+
+        after do |example|
+          example.metadata[:response][:examples] = { 'application/json' => JSON.parse(response.body, symbolize_names: true) }
+        end
+        run_test!
+      end
+    end
 
     post('create user') do
       tags 'User'
       consumes 'application/json'
-      parameter name: :user, in: :body, schema: {
+      parameter name: :body, in: :body, schema: {
           type: :object,
-          properties: {
-              user[:name]: { type: :string },
-              username: { type: :string },
-              email: { type: :string },
-              password: { type: :string },
-              password_confirmation: { type: :string }
-          },
-          required: [ 'name', 'username', 'email', 'password', 'password_confirmation' ]
+            properties: {
+                  name: { type: :string },
+                  username: { type: :string },
+                  email: { type: :string },
+                  password: { type: :string },
+                  password_confirmation: { type: :string }
+
+            },
+            required: [ 'name', 'username', 'email', 'password', 'password_confirmation' ]
       }
 
       response(201, 'successful') do
-        let(:user) {
+        let(:body) {
           {user: {name: "aa", username:"aas", email: "x1wins@changwoo.net", password: "password123", password_confirmation: "password123"}}
         }
         run_test!
@@ -38,21 +40,25 @@ RSpec.describe 'Users API' do
     end
   end
 
-  # path '/users/{_username}' do
-  #   # You'll want to customize the parameter types...
-  #   parameter name: '_username', in: :path, type: :string, description: '_username'
-  #
-  #   get('show user') do
-  #     response(200, 'successful') do
-  #       let(:_username) { '123' }
-  #
-  #       after do |example|
-  #         example.metadata[:response][:examples] = { 'application/json' => JSON.parse(response.body, symbolize_names: true) }
-  #       end
-  #       run_test!
-  #     end
-  #   end
-  #
+  path '/users/{_username}' do
+    get('show user') do
+      tags 'User'
+      consumes 'application/json'
+      parameter name: :_username, :in => :path, :type => :string
+      response(200, 'successful') do
+        let(:user){
+          create(:user)
+        }
+        let(:_username) { user.username }
+        run_test!
+      end
+
+      response(404, 'not found') do
+        let(:_username) { "hello" }
+        run_test!
+      end
+    end
+
   #   patch('update user') do
   #     response(200, 'successful') do
   #       let(:_username) { '123' }
@@ -85,5 +91,5 @@ RSpec.describe 'Users API' do
   #       run_test!
   #     end
   #   end
-  # end
+  end
 end
