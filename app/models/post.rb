5f8e60283b4fddd970adafd8febdf8e6e09a6a88
Chang-Woo Rhee <x1wins@changwoo.net>
2020-01-07 14:08:27 -0800

    Added rails generate migration ChangePublishedDefaultToPosts published:boolean

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,6 @@ class Post < ApplicationRecord
   belongs_to :user
   has_many :comments
   validates :body, presence: true
+  scope :published, -> { where(published: true) }
+  scope :by_date, -> { order('created_at DESC, id DESC') }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_26_233004) do
+ActiveRecord::Schema.define(version: 2020_01_07_220544) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,7 +29,7 @@ ActiveRecord::Schema.define(version: 2019_12_26_233004) do
   create_table "posts", force: :cascade do |t|
     t.string "body"
     t.bigint "user_id", null: false
-    t.boolean "published"
+    t.boolean "published", default: true
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_posts_on_user_id"
