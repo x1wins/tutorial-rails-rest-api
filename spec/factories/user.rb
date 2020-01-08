16c56a0381c644571917760c661cf97b5855aa88
Chang-Woo Rhee <x1wins@changwoo.net>
2020-01-07 22:11:09 -0800

    Added unique for Failure/Error: user  { create(:user) }
              ActiveRecord::RecordInvalid:
                Validation failed: Username has already been taken

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -2,7 +2,7 @@
 FactoryBot.define do
   factory :user do
     name    { Faker::Name.first_name }
-    username    { Faker::Name.last_name }
+    username    { Faker::Name.unique.last_name }
     email    { Faker::Internet.email }
     password { Faker::Internet.password }
     password_confirmation { password }
