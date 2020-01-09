15e184323ab2bf1bc7376bc074be4d131f907b02
Chang-Woo Rhee <x1wins@changwoo.net>
2020-01-09 10:22:12 -0800

    Added using login_params

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -3,8 +3,8 @@ class AuthenticationController < ApplicationController
 
   # POST /auth/login
   def login
-    @user = User.find_by_email(params[:email])
-    if @user&.authenticate(params[:password])
+    @user = User.find_by_email(login_params[:email])
+    if @user&.authenticate(login_params[:password])
       token = JsonWebToken.encode(user_id: @user.id)
       time = Time.now + 24.hours.to_i
       render json: { token: token, exp: time.strftime("%m-%d-%Y %H:%M"),
