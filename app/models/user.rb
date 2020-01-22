34baf92e8313f6e1b9de52e5c578c7a4dc64d90d
Chang-Woo Rhee <x1wins@changwoo.net>
2020-01-21 16:14:17 -0800

    Added role http://railscasts.com/episodes/189-embedded-association https://github.com/ryanb/cancan/wiki/Role-Based-Authorization

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,24 @@ class User < ApplicationRecord
   validates :password,
             length: { minimum: 6 },
             if: -> { new_record? || !password.nil? }
+
+  named_scope :with_role, lambda { |role| {:conditions => "roles_mask & #{2**ROLES.index(role.to_s)} > 0"} }
+
+  ROLES = %w[admin moderator author banned].freeze
+
+  def roles=(roles)
+    self.roles_mask = (roles & ROLES).map { |r| 2**ROLES.index(r) }.sum
+  end
+
+  def roles
+    ROLES.reject { |r| ((roles_mask || 0) & 2**ROLES.index(r)).zero? }
+  end
+
+  def role_symbols
+    roles.map(&:to_sym)
+  end
+
+  def is?(role)
+    roles.include?(role.to_s)
+  end
 end
