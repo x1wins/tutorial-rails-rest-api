8181de26c84c80773928c8972b6b325d00590850
Chang-Woo Rhee <x1wins@changwoo.net>
2020-01-22 13:38:54 -0800

    Deleted root path (404 not found) http://localhost:3000/

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root to: proc { [404, {}, ["Not found."]] }
+
   resources :categories
   mount Rswag::Ui::Engine => '/api-docs'
   mount Rswag::Api::Engine => '/api-docs'
