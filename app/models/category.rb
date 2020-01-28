d1045e7bf14d96d27658730253caa9b889a3b103
Chang-Woo Rhee <x1wins@changwoo.net>
2020-01-28 13:13:41 -0800

    Added posts in category

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,6 @@
 class Category < ApplicationRecord
   belongs_to :user
+  has_many :posts
   scope :published, -> { where(published: true) }
   scope :by_date, -> { order('created_at DESC, id DESC') }
   validates :title, presence: true
--- app/serializers/category_serializer.rb
+++ app/serializers/category_serializer.rb
@@ -1,4 +1,10 @@
 class CategorySerializer < ActiveModel::Serializer
   attributes :id, :title, :body
   has_one :user
+  has_many :posts
+  def posts
+    post_page = instance_options[:post_page].presence || 1
+    post_per = instance_options[:post_per].presence || 10
+    object.posts.published.by_date.page(post_page).per(post_per)
+  end
 end
