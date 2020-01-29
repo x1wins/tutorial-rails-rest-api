7f74e17ee0fe9ea0ab28275a9983cfe988caf238
Chang-Woo Rhee <x1wins@changwoo.net>
2020-01-28 16:19:26 -0800

    Added self.build_json object

--- lib/pagination.rb
+++ lib/pagination.rb
@@ -1,13 +1,16 @@
-# my_app/app/serializers/pagination_serializer.rb
+# /lib/pagination.rb
 class Pagination
-  def self.build object
-    meta = {
+  def self.build_json object
+    ob_name = object.name.downcase + 's'
+    json = Hash.new
+    json[ob_name] = object
+    json[:pagination] = {
         current_page: object.current_page,
         next_page: object.next_page,
         prev_page: object.prev_page,
         total_pages: object.total_pages,
         total_count: object.total_count
     }
-    return meta
+    return json
   end
 end
