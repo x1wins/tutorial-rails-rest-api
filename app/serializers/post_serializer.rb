1c638f6320bebac4d16e9d5f045b576d5d07988d
Chang-Woo Rhee <x1wins@changwoo.net>
2020-01-29 11:53:21 -0800

    Added has_many_model_page, has_many_model_per

--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -4,7 +4,7 @@ class PostSerializer < ActiveModel::Serializer
   has_many :comments
   def comments
     comment_page = instance_options[:comment_page].presence || 1
-    comment_per = instance_options[:comment_per].presence || 0
+    comment_per = instance_options[:comment_per].presence || 10
     object.comments.published.by_date.page(comment_page).per(comment_per)
   end
 end
--- lib/pagination.rb
+++ lib/pagination.rb
@@ -1,9 +1,9 @@
 # /lib/pagination.rb
 class Pagination
-  def self.build_json object
+  def self.build_json object, has_many_model_page, has_many_model_per
     ob_name = object.name.downcase
     json = Hash.new
-    json[ob_name] = ActiveModel::SerializableResource.new(object.to_a).to_json
+    json[ob_name] = ActiveModel::SerializableResource.new(object.to_a, comment_page: has_many_model_page, comment_per: has_many_model_per)
     json[:pagination] = {
         current_page: object.current_page,
         next_page: object.next_page,
