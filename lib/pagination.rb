51566a19bf378ccd820ceafde366370a24a0dc6b
Chang-Woo Rhee <x1wins@changwoo.net>
2020-01-29 13:40:16 -0800

    Added pluralize in Pagination for object name

--- lib/pagination.rb
+++ lib/pagination.rb
@@ -1,7 +1,7 @@
 # /lib/pagination.rb
 class Pagination
   def self.build_json object, nested_page = 1, nested_per = 10
-    ob_name = object.name.downcase
+    ob_name = object.name.downcase.pluralize
     json = Hash.new
     json[ob_name] = ActiveModel::SerializableResource.new(object.to_a, nested_page: nested_page, nested_per: nested_per)
     json[:pagination] = {
