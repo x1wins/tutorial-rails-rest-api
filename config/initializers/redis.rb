8351b0c88ae1f47c1dbdf8f04c989031c6719b51
Chang-Woo Rhee <x1wins@changwoo.net>
2020-01-31 12:02:52 -0800

    Changed redis port to 7001

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,3 +1,4 @@
 # config/initializers/redis.rb
 
-$redis = Redis::Namespace.new("tutorial_post", :redis => Redis.new)
+# $redis = Redis::Namespace.new("tutorial_post", :redis => Redis.new, :host => 'localhost', :port => 7001)
+$redis = Redis.new(:host => '127.0.0.1', :port => 7001)
