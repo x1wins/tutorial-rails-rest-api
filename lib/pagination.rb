b42ee54f040d6d7f2115c41802b9c0282b245bdd
Chang-Woo Rhee <x1wins@changwoo.net>
2020-01-30 16:36:04 -0800

    Added self.information

--- lib/pagination.rb
+++ lib/pagination.rb
@@ -1,17 +1,20 @@
 # /lib/pagination.rb
 class Pagination
-  def self.build_json object, pagaination_param = {}
-    ob_name = object.name.downcase.pluralize.to_sym
-    json = Hash.new
-    json[ob_name] = ActiveModelSerializers::SerializableResource.new(object.to_a, pagaination_param: pagaination_param)
-    pagination_name = "#{ob_name}_pagination".to_sym
-    json[pagination_name] = {
-        current_page: object.current_page,
-        next_page: object.next_page,
-        prev_page: object.prev_page,
-        total_pages: object.total_pages,
-        total_count: object.total_count
+  def self.information array
+    {
+        current_page: array.current_page,
+        next_page: array.next_page,
+        prev_page: array.prev_page,
+        total_pages: array.total_pages,
+        total_count: array.total_count
     }
-    return json
+  end
+  def self.build_json array, pagaination_param = {}
+    ob_name = array.name.downcase.pluralize.to_sym
+    json = Hash.new
+    json[ob_name] = ActiveModelSerializers::SerializableResource.new(array.to_a, pagaination_param: pagaination_param)
+    pagination_name = "#{ob_name}_pagination".to_sym
+    json[pagination_name] = self.information array
+    json
   end
 end
