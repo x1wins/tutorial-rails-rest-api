2bbb780a143b6b575bb691c7e8f6476ea6d6fee1
Chang-Woo Rhee <x1wins@changwoo.net>
2020-01-31 14:03:59 -0800

    Added redis namespece

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,4 +1,3 @@
 # config/initializers/redis.rb
 
-# $redis = Redis::Namespace.new("tutorial_post", :redis => Redis.new, :host => 'localhost', :port => 7001)
-$redis = Redis.new(:host => '127.0.0.1', :port => 7001)
+$redis = Redis::Namespace.new("tutorial_post", :redis => Redis.new(:host => '127.0.0.1', :port => 7001))
