7b264794a42e3211e088909574f131bf8927b62e
Chang-Woo Rhee <x1wins@changwoo.net>
2020-02-05 17:05:58 -0800

    Added redis.yml

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,3 +1,5 @@
 # config/initializers/redis.rb
+redis_config = YAML.load_file(Rails.root + 'config/redis.yml')[Rails.env]
+$redis = Redis::Namespace.new(redis_config['namespace'], redis: Redis.new(host: redis_config['host'], port: redis_config['port']))
 
-$redis = Redis::Namespace.new("tutorial_post", :redis => Redis.new(:host => 'redis', :port => 6379))
+
