ca0ad61ec7040b0ac2e2f41c553084f7a706efce
Chang-Woo Rhee <x1wins@changwoo.net>
2020-02-07 13:59:35 -0800

    Added files array attribute

--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -1,7 +1,15 @@
 class PostSerializer < ActiveModel::Serializer
-  attributes :id, :body, :comments_pagination
+  include Rails.application.routes.url_helpers
+  attributes :id, :body, :files, :comments_pagination
   has_one :user
   has_many :comments
+  def files
+    return unless object.files.attachments
+    file_urls = object.files.map do |file|
+      rails_blob_url(file)
+    end
+    file_urls
+  end
   def comments
     comment_page = instance_options.dig(:pagaination_param, :comment_page).presence.to_i || 1
     comment_per = instance_options.dig(:pagaination_param, :comment_per).presence.to_i || 0
