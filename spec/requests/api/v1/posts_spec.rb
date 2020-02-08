8b4848ba529e8eee76016d54cc13ef7eb73bd6b0
Chang-Woo Rhee <x1wins@changwoo.net>
2020-02-07 16:42:09 -0800

    Added multipart form Post for rswag

--- spec/requests/api/v1/posts_spec.rb
+++ spec/requests/api/v1/posts_spec.rb
@@ -186,7 +186,6 @@ RSpec.describe 'Posts API', type: :request do
       tags 'Post'
       security [Bearer: []]
       consumes 'application/json'
-      parameter name: :files, :in => :formData, :type => :file, required: true
       parameter name: :Authorization, in: :header, type: :string, description: 'JWT token for Authorization'
       parameter name: :body, in: :body, required: true, schema: {
           type: :object,
@@ -534,4 +533,35 @@ RSpec.describe 'Posts API', type: :request do
       end
     end
   end
+
+  path '/api/v1/posts/' do
+    post('multipart form') do
+      tags 'multipart form Post'
+      security [Bearer: []]
+      consumes 'multipart/form-data'
+      parameter name: :Authorization, in: :header, type: :string, description: 'JWT token for Authorization'
+      parameter name: 'post[body]', in: :formData, type: :string, required: true
+      parameter name: 'post[category_id]', in: :formData, type: :integer, required: true
+      parameter name: 'post[files][]', in: :formData, type: :file
+      parameter name: 'post[files][]', in: :formData, type: :file
+      response(201, 'Successful') do
+        let(:user){
+          create(:user)
+        }
+        let(:build_post){
+          build(:post)
+        }
+        let(:Authorization) { authenticated_header(user: user) }
+        let(:'post[body]') { build_post.body }
+        let(:'post[category_id]') { build_post.category.id }
+        let(:'post[files][]') { Rack::Test::UploadedFile.new(Rails.root.join("spec/factories/sample.txt")) }
+
+        after do |example|
+          example.metadata[:response][:examples] = { 'application/json' => JSON.parse(response.body, symbolize_names: true) }
+        end
+        run_test!
+      end
+    end
+  end
+
 end
