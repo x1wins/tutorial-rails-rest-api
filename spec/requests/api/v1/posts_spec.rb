fd238b2a754edcce3286cb0494bf6a3ca30d7de2
Chang-Woo Rhee <x1wins@changwoo.net>
2020-02-07 16:28:03 -0800

    Revert consumes 'application/json', file path

--- spec/requests/api/v1/posts_spec.rb
+++ spec/requests/api/v1/posts_spec.rb
@@ -8,6 +8,7 @@ RSpec.describe 'Posts API', type: :request do
     get('list posts') do
       tags 'Post'
       security [Bearer: []]
+      consumes 'application/json'
       parameter name: :Authorization, in: :header, type: :string, description: 'JWT token for Authorization'
       parameter name: :category_id, in: :query, type: :integer, default: '1', description: 'Category Id'
       parameter name: :page, in: :query, type: :integer, default: '1', description: 'Page number'
@@ -184,7 +185,7 @@ RSpec.describe 'Posts API', type: :request do
     post('create post') do
       tags 'Post'
       security [Bearer: []]
-      consumes 'multipart/form-data'
+      consumes 'application/json'
       parameter name: :files, :in => :formData, :type => :file, required: true
       parameter name: :Authorization, in: :header, type: :string, description: 'JWT token for Authorization'
       parameter name: :body, in: :body, required: true, schema: {
@@ -201,7 +202,6 @@ RSpec.describe 'Posts API', type: :request do
       }
       produces 'application/json'
       response(201, 'Successful') do
-        let(:files) { Rack::Test::UploadedFile.new(Rails.root.join("spec/factories/sample.txt")) }
         let(:user){
           create(:user)
         }
