1c239a0b8cfa24eb8a21690b5cab9d7199264690
Chang-Woo Rhee <x1wins@changwoo.net>
2020-02-07 16:50:53 -0800

    Craeted upload_spec for rswag

--- spec/requests/api/v1/posts_spec.rb
+++ spec/requests/api/v1/posts_spec.rb
@@ -534,34 +534,4 @@ RSpec.describe 'Posts API', type: :request do
     end
   end
 
-  path '/api/v1/posts/' do
-    post('multipart form') do
-      tags 'multipart form Post'
-      security [Bearer: []]
-      consumes 'multipart/form-data'
-      parameter name: :Authorization, in: :header, type: :string, description: 'JWT token for Authorization'
-      parameter name: 'post[body]', in: :formData, type: :string, required: true
-      parameter name: 'post[category_id]', in: :formData, type: :integer, required: true
-      parameter name: 'post[files][]', in: :formData, type: :file
-      parameter name: 'post[files][]', in: :formData, type: :file
-      response(201, 'Successful') do
-        let(:user){
-          create(:user)
-        }
-        let(:build_post){
-          build(:post)
-        }
-        let(:Authorization) { authenticated_header(user: user) }
-        let(:'post[body]') { build_post.body }
-        let(:'post[category_id]') { build_post.category.id }
-        let(:'post[files][]') { Rack::Test::UploadedFile.new(Rails.root.join("spec/factories/sample.txt")) }
-
-        after do |example|
-          example.metadata[:response][:examples] = { 'application/json' => JSON.parse(response.body, symbolize_names: true) }
-        end
-        run_test!
-      end
-    end
-  end
-
 end
