2ad38dc3d486f0a255f2e0ae522aa653a76578d7
Chang-Woo Rhee <x1wins@changwoo.net>
2020-02-11 11:13:16 -0800

    Deleled action mailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -29,9 +29,9 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  # config.action_mailer.raise_delivery_errors = false
 
-  config.action_mailer.perform_caching = false
+  # config.action_mailer.perform_caching = false
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
@@ -51,4 +51,5 @@ Rails.application.configure do
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
   routes.default_url_options[:host] = 'localhost:3000'
+
 end
