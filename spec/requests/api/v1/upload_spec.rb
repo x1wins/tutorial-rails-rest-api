3b7aa4eff434580787b3b461b5be6c89eed3108c
Chang-Woo Rhee <x1wins@changwoo.net>
2020-02-13 00:16:08 -0800

    Added uploaded file counting test

--- spec/requests/api/v1/upload_spec.rb
+++ spec/requests/api/v1/upload_spec.rb
@@ -6,6 +6,9 @@ RSpec.describe 'Posts API', type: :request do
   let(:uploadfile){
     Rack::Test::UploadedFile.new(Rails.root.join("spec/factories/sample.txt"))
   }
+  let(:second_uploadfile){
+    Rack::Test::UploadedFile.new(Rails.root.join("spec/factories/sample.txt"))
+  }
 
   path '/api/v1/posts/' do
     post('create multipart form') do
@@ -15,7 +18,7 @@ RSpec.describe 'Posts API', type: :request do
       parameter name: :Authorization, in: :header, type: :string, description: 'JWT token for Authorization'
       parameter name: 'post[body]', in: :formData, type: :string, required: true
       parameter name: 'post[category_id]', in: :formData, type: :integer, required: true
-      parameter name: 'post[files][]', in: :formData, type: :file
+      parameter name: 'post[files]', in: :formData, type: :file
       response(201, 'Successful') do
         let(:user){
           create(:user)
@@ -26,10 +29,12 @@ RSpec.describe 'Posts API', type: :request do
         let(:Authorization) { authenticated_header(user: user) }
         let(:'post[body]') { build_post.body }
         let(:'post[category_id]') { build_post.category.id }
-        let(:'post[files][]') { uploadfile }
+        let(:'post[files]') { [uploadfile, second_uploadfile] }
 
         after do |example|
           example.metadata[:response][:examples] = { 'application/json' => JSON.parse(response.body, symbolize_names: true) }
+          r = JSON.parse(response.body, symbolize_names: true)
+          expect(r[:files].count).to be == 2
         end
         run_test!
       end
@@ -44,10 +49,12 @@ RSpec.describe 'Posts API', type: :request do
         let(:Authorization) { authenticated_header(user: user) }
         let(:'post[body]') { build_post.body }
         let(:'post[category_id]') { build_post.category.id }
-        let(:'post[files][]') { }
+        let(:'post[files]') { }
 
         after do |example|
           example.metadata[:response][:examples] = { 'application/json' => JSON.parse(response.body, symbolize_names: true) }
+          r = JSON.parse(response.body, symbolize_names: true)
+          expect(r[:files].count).to be == 0
         end
         run_test!
       end
@@ -62,13 +69,13 @@ RSpec.describe 'Posts API', type: :request do
       parameter name: :Authorization, in: :header, type: :string, description: 'JWT token for Authorization'
       parameter name: 'id', in: :path, type: :string, description: 'id'
       parameter name: 'post[body]', in: :formData, type: :string, required: true
-      parameter name: 'post[files][]', in: :formData, type: :file
+      parameter name: 'post[files]', in: :formData, type: :file
       response(200, 'Successful') do
         let(:user){
           create(:user)
         }
         let(:post){
-          create(:post)
+          create(:post, files: [ uploadfile, second_uploadfile ])
         }
         let(:build_post){
           build(:post)
@@ -76,10 +83,12 @@ RSpec.describe 'Posts API', type: :request do
         let(:Authorization) { authenticated_header(user: post.user) }
         let(:id) { post.id }
         let(:'post[body]') { build_post.body }
-        let(:'post[files][]') { uploadfile }
+        let(:'post[files]') { [uploadfile] }
 
         after do |example|
           example.metadata[:response][:examples] = { 'application/json' => JSON.parse(response.body, symbolize_names: true) }
+          r = JSON.parse(response.body, symbolize_names: true)
+          expect(r[:files].count).to be == 3
         end
         run_test!
       end
